644d133b2499ab51317d5239efb6511c617b22c8
CdnCarvalho <claudenirfer@gmail.com>
2026-04-09 19:09:54 -0300

    Acrescenta IFs encadeados e aninhados

--- aula04/exemplo01.py
+++ aula04/exemplo01.py
@@ -33,7 +33,53 @@ else:
 usuario = input('Nome: ').lower()
 senha = input('Senha: ')
 
-if usuario == "admin" or senha == "1234":
+if usuario == "admin" and senha == "1234":
     print('Login realizado com sucesso')
 else:
     print('Usuário ou senha incorretos')
+
+
+# Exemplo IFs Encadado
+nota = 3
+if nota >= 9:
+    print('A')
+elif nota >= 7:
+    print('B')
+elif nota >= 5:
+    print('C')
+elif nota >= 3:
+    print('D')
+else:
+    print('E')
+
+
+# Exemplo IFs Não Encadeado
+nota = 10
+if nota >= 9:
+    print('A')
+
+if nota >= 7:
+    print('B')
+
+if nota >= 5:
+    print('C')
+
+if nota >= 3:
+    print('D')
+else:
+    print('E')
+
+
+# Ifs aninhados
+nota = float(input('Insira a nota: '))
+frequencia = float(input('Informe a frequência: '))
+
+if nota >= 7:
+    # Aprovado por nota, mas precisa checar a frequência
+    if frequencia >= 75:
+        print('Aluno aprovado por Nota e Frequência')
+    else:
+        print('Reprovado por freqência baixa')
+else:
+    print('Reprovado por nota baixa. ')
+    
